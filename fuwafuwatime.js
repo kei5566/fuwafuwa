8a7e131caaa9dc1f19f7642d05df680ab23b7767
murakami <murakami@interlogic.jp>
2017-06-09 18:52:15 +0900

    デフォルトの値修正

--- fuwafuwatime.js
+++ fuwafuwatime.js
@@ -2,7 +2,7 @@
     $.fn.fuwafuwa = function(options) {
         var defaults = {
             animation:1000,
-            settime:1000
+            settime:0
         };
         var setting = $.extend(defaults, options);
         $(this).delay(setting.settime).css({display:'block',opacity:'0'}).animate({opacity:'1'},setting.animation);
@@ -14,7 +14,7 @@
     $.fn.fuwafuwaout = function(options) {
         var defaults = {
             animation:1000,
-            settimeout:1000
+            settimeout:0
         };
         var timesetting = $.extend(defaults, options);
         $(this).delay(timesetting.settimeout).animate({opacity:'0'},timesetting.animation);
