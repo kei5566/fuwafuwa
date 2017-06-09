9407de834bbcb9a8756cb83e25e111cae1885cca
murakami <murakami@interlogic.jp>
2017-06-09 18:51:48 +0900

    fuwafuwa fuwafuwaout メソッド作成

--- fuwafuwatime.js
+++ fuwafuwatime.js
@@ -2,10 +2,23 @@
     $.fn.fuwafuwa = function(options) {
         var defaults = {
             animation:1000,
-            settime:0
+            settime:1000
         };
         var setting = $.extend(defaults, options);
         $(this).delay(setting.settime).css({display:'block',opacity:'0'}).animate({opacity:'1'},setting.animation);
         return(this);
+    };
+}( jQuery ));
+
+(function ( $ ) {
+    $.fn.fuwafuwaout = function(options) {
+        var defaults = {
+            animation:1000,
+            settimeout:1000
+        };
+        var timesetting = $.extend(defaults, options);
+        $(this).delay(timesetting.settimeout).animate({opacity:'0'},timesetting.animation);
+        return(this);
+
     };
 }( jQuery ));
